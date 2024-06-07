f543392d0a74b287bde7312d90cabac323458ab3
Il tuo nome <la_tua_email@example.com>
2024-06-07 17:31:27 +0200

    Created AppTitle component and linked to the Appvue

---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data(){
+
+return {
+  message: "La mia prima app di vue"
+}
+}
+};
+
+</script>
+
+<template>
+
+
+    <h1 class="textColor">{{ message }}</h1>
+  
+
+</template>
+
+<style scoped>
+  
+.textColor {
+  color: red;
+  text-transform: capitalize;
+  
+}
+h1 {
+  font-size: 5.5rem;
+  line-height: 1.1;
+}
+
+
+</style>
