da23848220c7b73a51694813ba64278453c86d20
Il tuo nome <la_tua_email@example.com>
2024-06-08 13:00:31 +0200

    Added jumbo

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AppTitle from './components/AppTitle.vue'
+import Jumbo from './components/Jumbo.vue'
+
 </script>
 
 <template>
   
     <AppTitle/>
-    
+    <Jumbo/>
 </template>
 
 <style scoped>
---
+++ my-vue-app/src/components/Jumbo.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+    <div class="d-flex jumbo-container align-items-center justify-content-center w-100 bg-warning" >
+        <img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/04/Goku-Gohan-Kamehameha.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5" alt="" class="w-75 jumbo">
+    </div>
+</template>
+
+
+
+<style scoped>
+
+.jumbo{
+ min-height: 200px;
+ min-width: 200px;
+}
+
+
+
+</style>
